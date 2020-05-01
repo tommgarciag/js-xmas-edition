a10b5f82540d49e7e5d2b63c117c84b8f0cb29cb
tommgarciag <56352971+tommgarciag@users.noreply.github.com>
2020-05-01 17:01:00 -0300

    crea validacion para el campo descripcion regalo

--- js/main.js
+++ js/main.js
@@ -22,18 +22,31 @@ function validarCiudad(ciudad){
     return "";
 }
 
+function validarDescripcionRegalo(descripcionRegalo){
+    if (descripcionRegalo.length === 0){
+        return "Este campo debe tener al menos 1 caracter";
+    }else if (descripcionRegalo.length >= 100) {
+        return "Este campo debe tener menos de 100 caracteres";
+    }else if (!/^[A-z0-9,\. ]+$/.test(descripcionRegalo)){         
+        return "Este campo solo puede contener numeros y letras";
+    }else{ return "" ;}
+}
+
 
 function validarFormulario(event){
     const $form = document.querySelector("#carta-a-santa");
 
     const nombre = $form.nombre.value;
     const ciudad = $form.ciudad.value;
+    const descripcionRegalo = $form["descripcion-regalo"].value;
     const errorNombre = validarNombre(nombre);
     const errorCiudad = validarCiudad(ciudad);
+    const errorDescripcionRegalo = validarDescripcionRegalo(descripcionRegalo);
 
     const errores = {
         nombre: errorNombre,
         ciudad: errorCiudad,
+        "descripcion-regalo": errorDescripcionRegalo,
     }
 
     manejarErrores(errores);
