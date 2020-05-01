11c4fe22fc4ffbd7de2027fadda44f05d0b6ee49
tommgarciag <56352971+tommgarciag@users.noreply.github.com>
2020-05-01 16:38:49 -0300

    crea prueba para la validacion del nombre

--- js/pruebas.js
+++ js/pruebas.js
@@ -1,15 +1,22 @@
 function probarValidarNombre() {
-  console.assert(
-      validarNombre('') === 'Este campo debe tener al menos 1 caracter',
-      'Validar nombre no validó que el nombre no sea vacío',
-  );
-
-  console.assert(
-      validarNombre(
-          '111111111111111111111111111111111111111111111111111111111111111111111111111111111111111') ===
-      'Este campo debe tener menos de 50 caracteres',
-      'Validar nombre no validó que el nombre sea menor a 50 caracteres',
-  );
-}
-
-probarValidarNombre();
+    console.assert(
+        validarNombre('') === 'Este campo debe tener al menos 1 caracter',
+        'Validar nombre no validó que el nombre no sea vacío',
+    );
+  
+    console.assert(
+        validarNombre(
+            '111111111111111111111111111111111111111111111111111111111111111111111111111111111111111') ===
+        'Este campo debe tener menos de 50 caracteres',
+        'Validar nombre no validó que el nombre sea menor a 50 caracteres',
+    );
+  
+    console.assert(
+        validarNombre('sdfs3312') === 'Este campo debe contener solo letras',
+        'validarNombre no valido que el campo contenga solo letras',
+    );
+  
+    console.assert(validarNombre('tomas') === '', 'validarNombre fallo con un nombre valido',);
+  }
+  
+  probarValidarNombre();
